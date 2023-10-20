f7bc177d452795112c7b75abec230b0052d0592d
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2023-10-20 21:51:45 +0300

    Update helloworld.sol

--- helloworld.sol
+++ helloworld.sol
@@ -1,7 +1,13 @@
+// The compiler version must be 
+// greater than or equal to 0.8.19 
+// and less than 0.9.0
 // حدد إصدار المترجم الذي ستستخدمه
-// يجب أن يكون إصدار المترجم أكبر من أو يساوي 0.8.19 وأقل من 0.9.0
+// يجب أن يكون إصدار المترجم أكبر من 
+أو يساوي 0.8.19 وأقل من 0.9.0
+
 pragma solidity ^0.8.19;
 
+// 
 // HelloWorld قم بإنشاء عقد بإسم
 contract HelloWorld {
 
